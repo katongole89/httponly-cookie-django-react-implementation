8daf984b69a27ac40d045851e2c862b10ac53e98
katongole89 <katongolerichard089@gmail.com>
2022-11-11 11:38:57 +0300

    added simple jwt, some code for auth in settings

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import './login.css'
+import './Login.css'
 
 const Login = () => {
     return(
