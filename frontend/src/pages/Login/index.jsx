e87f4bf751a4217600b6c7f345a2de83d29cf492
katongole89 <katongolerichard089@gmail.com>
2022-11-11 11:14:51 +0300

    added input components for the login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './login.css'
 
 const Login = () => {
     return(
-        <div>
-            Login page
+        <div className="container">
+            <form>
+                <div style={{display:'flex', flexDirection:'column'}}>
+                    <div>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    </div>
+                    <div style={{maginTop:'10px'}}>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    </div>
+                    <div>
+                        <Button variant="contained">Login</Button>
+                    </div>
+                    
+                </div>
+                
+            </form>
         </div>
+        
+            
     )
 }
 
