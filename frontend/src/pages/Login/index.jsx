b625fe7f22e40b54dbf9768696b66746405b93cd
katongole89 <katongolerichard089@gmail.com>
2022-11-11 12:00:18 +0300

    added state and handlechange for input fields on the login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './Login.css'
 
 const Login = () => {
+
+    const [state, setState] = useState({
+        username:'',
+        password:''
+    })
+
+    const handleChange = (e) =>{
+        setState({...state, [e.target.name]:e.target.value})
+    }
+
+
+
+
     return(
         <div className="container">
             <form>
                 <div style={{display:'flex', flexDirection:'column'}}>
                     <div>
-                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined" name='username' />
                     </div>
                     <div style={{maginTop:'10px'}}>
-                        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined" name='password'/>
                     </div>
                     <div>
                         <Button variant="contained">Login</Button>
